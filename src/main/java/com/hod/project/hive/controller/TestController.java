4ae4830d030700d255891b7b88d1b52396512c1c
백봉준(bongjun.baek) <bongjun.baek@gmail.com>
2023-03-28 11:26:42 +0900

    [#2][Add] 가이드용 http 및 API 추가

--- src/main/java/com/hod/project/hive/controller/TestController.java
+++ src/main/java/com/hod/project/hive/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.hod.project.hive.controller;
 
+import com.hod.project.hive.common.vo.RequestParams;
+import com.hod.project.hive.common.vo.ResponseParams;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,27 @@ public class TestController {
     @PostMapping("/test")
     public ResponseEntity<Map<String, String>> methodPost() {
         return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/test/param")
+    public ResponseEntity<ResponseParams> methodPostWithMapParams(
+            @RequestParam String id,
+            @RequestParam(value = "data", required = false, defaultValue = "DEFAULT VALUE") String data
+    ) {
+
+        ResponseParams params = new ResponseParams();
+        params.setResult("200");
+        params.setReason("id: " + id + ", data: " + data);
+        return ResponseEntity.ok(params);
+    }
+
+    @PostMapping("/test/object")
+    public ResponseEntity<ResponseParams> methodPostWithObjectParams(
+            @RequestBody RequestParams requestParams
+    ) {
+        ResponseParams params = new ResponseParams(ResponseParams.Status.CODE_200);
+        params.setReason(requestParams.toString());
+        return ResponseEntity.ok(params);
     }
 
     @GetMapping("/test")
